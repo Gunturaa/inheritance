6c855b505b00b1afc13f2ccaca4875beb1465c85
Gunturaa <gunturtriatmaja@students.amikom.ac.id>
2023-06-04 22:44:04 +0700

    commit ke-8

---
+++ inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            person person1 = new person("Guntur ganteng", 19);
+            person1.Infoperson();
+
+         
+            Karyawan karyawan = new Karyawan("Haitsam", "Manusia Silver");
+            karyawan.InfoKaryawan();
+
+            Console.ReadLine();
+        }
+    }
+}
